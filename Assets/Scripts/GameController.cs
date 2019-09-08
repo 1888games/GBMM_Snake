b08131923b777c430e7472730e037a268d402d90
Nick Sherman <nick@1888games.co.uk>
2019-09-08 21:42:35 +0100

    Upload to Guthub

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,285 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviourSingleton<GameController>
+{
+
+
+	public float SnakeSpeed = 1f;
+	public float StartSpeed = 0.5f;
+	public float ChangePerFood = 0.04f;
+	public float MaxSpeed = 0.2f;
+	
+
+	public Vector2Int SnakeDirection;
+	public Vector2Int SnakePosition;
+	public List<Vector2Int> SnakeTrail;
+
+	public bool GameReady = false;
+
+	public int FoodCollected = 0;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		Invoke ("Initialise", 0.05f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+		if (GameReady) {
+
+			CheckInput ();
+
+		}
+    	
+        
+    }
+
+
+	void CheckInput () {
+
+		if (Input.GetAxis ("Horizontal") > 0 && SnakeDirection != new Vector2Int(-1, 0)) {
+			SnakeDirection = new Vector2Int (1, 0);
+
+		}
+		
+		if (Input.GetAxis ("Horizontal") < 0 && SnakeDirection != new Vector2Int(1, 0)) {
+			SnakeDirection = new Vector2Int (-1, 0);
+
+		}
+		
+			
+		if (Input.GetAxis ("Vertical") > 0 && SnakeDirection != new Vector2Int(0, 1)){
+			SnakeDirection = new Vector2Int (0, -1);
+
+		}
+		
+		if (Input.GetAxis ("Vertical") < 0 && SnakeDirection != new Vector2Int(0, -1)) {
+			SnakeDirection = new Vector2Int (0, 1);
+
+		}
+
+
+	}
+
+
+	void PositionFood () {
+
+		List<Vector2Int> Tried = new List<Vector2Int> ();
+
+		bool ok = false;
+		int attempts = 0;
+
+		while (ok == false && attempts < 100) {
+
+			int x = Random.Range (0, ScreenController.Instance.Columns);
+			int y = Random.Range (0, ScreenController.Instance.Rows);
+
+			if (Tried.Contains (new Vector2Int (x, y)) == false) {
+
+				if (ScreenController.Instance.GetPixelName (x, y) == "None" && Vector2Int.Distance(SnakePosition, new Vector2Int(x,y)) > 3) {
+
+					ScreenController.Instance.LightPixel (x, y, "Food");
+					//Debug.Log ("FOOD: " + x + " / " + y + " ATT: " + attempts);
+		
+					ok = true;
+
+				} else {
+					Tried.Add (new Vector2Int (x, y));
+				}
+
+			}
+
+			attempts++;
+
+			//Debug.Log (attempts);
+
+		}
+		
+		
+
+
+	}
+
+
+	void MoveSnake () {
+	
+		Vector2Int NextSnakePosition = SnakePosition + SnakeDirection;
+		
+		if (NextSnakePosition.x > ScreenController.Instance.Columns) {
+			NextSnakePosition.x = 0;
+		}
+
+		if (NextSnakePosition.x < 0) {
+			NextSnakePosition.x = ScreenController.Instance.Columns;
+		}
+		
+		if (NextSnakePosition.y > ScreenController.Instance.Rows) {
+			NextSnakePosition.y = 0;
+		}
+
+		if (NextSnakePosition.y < 0) {
+			NextSnakePosition.y = ScreenController.Instance.Rows;
+		}
+		
+		
+		string nextPixel = ScreenController.Instance.GetPixelName (NextSnakePosition.x, NextSnakePosition.y);
+		
+		DeleteSnake ();
+
+		if (nextPixel == "SnakeTail") {
+			ScreenController.Instance.ClearScreen (true);
+			Debug.Log ("GAME OVER!!!!!");
+			CancelInvoke ();
+
+			Invoke ("ResetGame", 3f);
+			return;
+		}
+
+
+
+		//Debug.Log (nextPixel);
+
+		
+
+
+		for (int i = SnakeTrail.Count - 1; i > 0; i--) {
+
+			SnakeTrail [i] = SnakeTrail [i - 1];
+		}
+		
+		SnakeTrail [0] = SnakePosition;
+
+		SnakePosition = NextSnakePosition;
+		
+		
+		
+
+		DrawSnake ();
+		
+		if (nextPixel == "Food") {
+
+			FoodCollected++;
+			ScreenController.Instance.PlayNote (112, 127, 250);
+		
+			AddTailPiece ();
+			//Debug.Log (FoodCollected + " Snake Length " + (SnakeTrail.Count + 1).ToString());
+			PositionFood ();
+			SnakeSpeed -= ChangePerFood;
+
+			if (SnakeSpeed < MaxSpeed) {
+				SnakeSpeed = MaxSpeed;
+			}
+			
+		}
+
+		Invoke ("MoveSnake", SnakeSpeed);
+
+	
+
+	}
+
+	void Initialise () {
+
+
+		ScreenController.Instance.Initialise ();
+		
+
+		ResetGame ();
+		
+	
+
+	}
+
+
+	void DeleteSnake () {
+		
+		ScreenController.Instance.HidePixel (SnakePosition.x, SnakePosition.y);
+
+		foreach (Vector2Int snakePiece in SnakeTrail) {
+
+			if (ScreenController.Instance.GetPixelName (snakePiece.x, snakePiece.y) == "SnakeTail") {
+
+				ScreenController.Instance.HidePixel (snakePiece.x, snakePiece.y);
+			}
+		}
+
+	}
+
+	void AddTailPiece () {
+
+		Vector2Int EndOfTail = SnakeTrail [SnakeTrail.Count - 1];
+
+		for (int x = -1; x < 2; x++) {
+
+			
+			for (int y = -1; y < 2; y++) {
+			
+				if (x != 0 && y != 0) {
+					continue;
+				}
+
+				Vector2Int Position = EndOfTail + new Vector2Int (x, y);
+
+				//Debug.Log (Position + " " + ScreenController.Instance.GetPixelName (Position.x, Position.y));
+
+				if (ScreenController.Instance.GetPixelName (Position.x, Position.y) == "None") {
+
+					SnakeTrail.Add (Position);
+					ScreenController.Instance.LightPixel (Position.x, Position.y, "SnakeTail");
+					return;
+				}
+
+			}
+		}
+
+	}
+
+	void DrawSnake () {
+	
+		
+
+		ScreenController.Instance.LightPixel (SnakePosition.x, SnakePosition.y, "SnakeHead");
+
+		foreach (Vector2Int snakePiece in SnakeTrail) {
+
+			if (ScreenController.Instance.GetPixelName (snakePiece.x, snakePiece.y) != "Food") {
+
+				ScreenController.Instance.LightPixel (snakePiece.x, snakePiece.y, "SnakeTail");
+			}
+		}
+
+	}
+
+	void ResetGame () {
+
+		SnakeTrail = new List<Vector2Int> ();
+
+		ScreenController.Instance.ClearScreen ();
+
+		SnakeTrail.Add (SnakePosition - SnakeDirection);
+		
+		SnakeSpeed = StartSpeed;
+
+		MoveSnake ();
+		FoodCollected = 0;
+		//AddTailPiece ();
+		PositionFood ();
+		
+		GameReady = true;
+
+	
+
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ Assets/Scripts/ScreenController.cs
@@ -0,0 +1,403 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Melanchall.DryWetMidi.Devices;
+using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Common;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using System;
+
+
+public class ScreenController : MonoBehaviourSingleton<ScreenController> {
+
+	public List<GameObject> GameboyGameObjects;
+
+	public Image [,] Backlights;
+
+	public int OutputMode = 0;
+
+	public Dictionary<int, bool> Channel1Outputs;
+	public Dictionary<int, bool> Channel15Outputs;
+	public Dictionary<int, bool> Channel16Outputs;
+
+	public bool Channel1Output = false;
+	public bool Channel15Output = false;
+	public bool Channel16Output = true;
+
+
+
+
+
+	private KeyCode [] keyCodes = {
+			KeyCode.Alpha0,
+			 KeyCode.Alpha1,
+			 KeyCode.Alpha2,
+			 KeyCode.Alpha3,
+			 KeyCode.Alpha4,
+			 KeyCode.Alpha5,
+			 KeyCode.Alpha6,
+			 KeyCode.Alpha7,
+			 KeyCode.Alpha8,
+			 KeyCode.Alpha9,
+		 };
+
+	public int Rows;
+	public int Columns;
+
+	public List<string> MidiDevices;
+
+	public OutputDevice MidiDevice;
+
+	int MidiDeviceID = -1;
+
+	Dictionary<string, int> Velocities;
+
+	public Text DeviceNameText;
+	public Text Output1Text;
+	public Text Output15Text;
+	public Text Output16Text;
+
+
+
+	// Start is called before the first frame update
+	void Start () {
+
+		MidiDevices = new List<string> ();
+
+		try {
+
+			foreach (var outputDevice in OutputDevice.GetAll ()) {
+				Debug.Log (outputDevice.Name);
+				MidiDevices.Add (outputDevice.Name);
+				MidiDeviceID = 0;
+			}
+
+			if (MidiDeviceID >= 0) {
+				MidiDevice = OutputDevice.GetByName (MidiDevices [MidiDeviceID]);
+				DeviceNameText.text = MidiDevices [MidiDeviceID] + " (" + MidiDeviceID + ")";
+			}
+
+		} catch (Exception e) {
+
+			Debug.Log (e.ToString ());
+		}
+
+		Velocities = new Dictionary<string, int> ();
+
+		Velocities.Add ("SnakeHead", 127);
+		Velocities.Add ("SnakeTail", 90);
+		Velocities.Add ("Food", 60);
+		Velocities.Add ("GameOver", 75);
+
+		Channel1Outputs = new Dictionary<int, bool> ();
+		Channel15Outputs = new Dictionary<int, bool> ();
+		Channel16Outputs = new Dictionary<int, bool> ();
+
+		Channel1Outputs.Add (0, false);
+		Channel1Outputs.Add (1, false);
+		Channel1Outputs.Add (2, true);
+		Channel1Outputs.Add (3, true);
+		Channel1Outputs.Add (4, true);
+
+		Channel15Outputs.Add (0, false);
+		Channel15Outputs.Add (1, true);
+		Channel15Outputs.Add (2, true);
+		Channel15Outputs.Add (3, true);
+		Channel15Outputs.Add (4, false);
+
+
+		Channel16Outputs.Add (0, true);
+		Channel16Outputs.Add (1, false);
+		Channel16Outputs.Add (2, true);
+		Channel16Outputs.Add (3, false);
+		Channel16Outputs.Add (4, true);
+
+		Output1Text.text = "1 - " + Channel1Outputs [OutputMode].ToString ();
+		Output15Text.text = "15 - " + Channel15Outputs [OutputMode].ToString ();
+		Output16Text.text = "16 - " + Channel16Outputs [OutputMode].ToString ();
+
+
+	}
+
+	public void Initialise () {
+
+		Backlights = new Image [8, 8];
+
+		float currentY = GameboyGameObjects [0].GetComponent<RectTransform> ().anchoredPosition.y;
+		int currentRow = 0;
+		int currentCol = 0;
+		float thisY = 0f;
+
+		foreach (GameObject go in GameboyGameObjects) {
+
+			Image image = go.GetComponent<Image> ();
+
+			thisY = go.GetComponent<RectTransform> ().anchoredPosition.y;
+
+			if (currentY != thisY) {
+				Columns = currentCol - 1;
+				currentCol = 0;
+				currentRow++;
+			}
+
+			currentY = thisY;
+
+
+			Backlights [currentCol, currentRow] = image;
+
+			currentCol++;
+
+			image.enabled = false;
+			image.name = "None";
+
+
+		}
+
+		Rows = currentRow;
+
+
+	}
+
+	public void ClearScreen (bool enabled = false) {
+
+
+		for (int x = 0; x <= Columns; x++) {
+
+			for (int y = 0; y <= Rows; y++) {
+
+				if (enabled == false) {
+					HidePixel (x, y);
+				} else {
+					LightPixel (x, y, "GameOver");
+				}
+			}
+
+
+		}
+
+
+	}
+
+
+
+	public string GetPixelName (int x, int y) {
+
+		if (PixelExists (x, y)) {
+
+			return Backlights [x, y].name;
+		}
+
+		return "OutOfBounds";
+
+	}
+
+
+
+	public void FlipPixel (int x, int y) {
+
+		if (PixelExists (x, y)) {
+
+			Backlights [x, y].enabled = !Backlights [x, y].enabled;
+		}
+
+
+	}
+
+
+
+
+	public int PixelToNoteNumber (int x, int y) {
+
+		return 36 + ((Rows - y) * 8) + x;
+
+
+
+	}
+
+	bool PixelExists (int x, int y) {
+
+		if (y < 0 || x < 0) {
+			return false;
+		}
+
+		if (x > Columns) {
+			return false;
+		}
+
+		if (y > Rows) {
+			return false;
+		}
+
+		Image image = Backlights [x, y];
+
+
+		return image != null;
+	}
+
+
+	public async void PlayNote (int noteNumber, int velocity, int stopAfter = 0) {
+
+		if (MidiDeviceID >= 0) {
+
+
+			NoteOnEvent NoteOn = new NoteOnEvent ();
+
+			NoteOn.Channel = (FourBitNumber)15;
+			NoteOn.NoteNumber = (SevenBitNumber)noteNumber;
+			NoteOn.Velocity = (SevenBitNumber)velocity;
+
+			if (Channel1Outputs [OutputMode]) {
+				NoteOn.Channel = (FourBitNumber)0;
+				MidiDevice.SendEvent (NoteOn);
+			}
+
+			if (Channel15Outputs [OutputMode]) {
+				NoteOn.Channel = (FourBitNumber)14;
+				MidiDevice.SendEvent (NoteOn);
+			}
+
+			if (Channel16Outputs [OutputMode]) {
+				NoteOn.Channel = (FourBitNumber)15;
+				MidiDevice.SendEvent (NoteOn);
+			}
+
+
+
+			Debug.Log ("Play " + noteNumber + " ID: " + MidiDeviceID);
+
+			if (stopAfter > 0) {
+				await Task.Delay (stopAfter);
+				StopNote (noteNumber);
+			}
+
+
+
+		}
+
+
+	}
+
+	void StopNote (int noteNumber) {
+
+		if (MidiDeviceID >= 0) {
+
+			NoteOffEvent NoteOff = new NoteOffEvent ();
+
+			NoteOff.Channel = (FourBitNumber)15;
+			NoteOff.NoteNumber = (SevenBitNumber)noteNumber;
+			NoteOff.Velocity = (SevenBitNumber)127;
+
+
+			if (Channel1Outputs [OutputMode]) {
+				NoteOff.Channel = (FourBitNumber)0;
+				MidiDevice.SendEvent (NoteOff);
+			}
+
+			if (Channel15Outputs [OutputMode]) {
+				NoteOff.Channel = (FourBitNumber)14;
+				MidiDevice.SendEvent (NoteOff);
+			}
+
+			if (Channel16Outputs [OutputMode]) {
+				NoteOff.Channel = (FourBitNumber)15;
+				MidiDevice.SendEvent (NoteOff);
+			}
+
+			Debug.Log ("Stop " + noteNumber + " ID: " + MidiDeviceID);
+
+
+
+
+		}
+
+
+	}
+
+
+
+
+
+	public void LightPixel (int x, int y, string name) {
+
+		if (PixelExists (x, y)) {
+
+			Backlights [x, y].enabled = true;
+			Backlights [x, y].name = name;
+
+			Debug.Log (name);
+
+			PlayNote (PixelToNoteNumber (x, y), Velocities [name]);
+
+			//Debug.Log ("Note on: " + PixelToNoteNumber (x, y));
+
+		}
+
+	}
+
+
+	public void HidePixel (int x, int y) {
+
+		if (PixelExists (x, y)) {
+
+			Backlights [x, y].enabled = false;
+			Backlights [x, y].name = "None";
+
+			StopNote (PixelToNoteNumber (x, y));
+
+			//Debug.Log ("Note off: " + PixelToNoteNumber (x, y));
+
+		}
+
+	}
+
+
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Backlights != null) {
+			int x = UnityEngine.Random.Range (0, Columns + 1);
+			int y = UnityEngine.Random.Range (0, Rows + 1);
+
+			//FlipPixel (x, y);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			OutputMode++;
+
+			if (OutputMode > Channel16Outputs.Count - 1) {
+				OutputMode = 0;
+			}
+
+			Output1Text.text = "1 - " + Channel1Outputs [OutputMode].ToString ();
+			Output15Text.text = "15 - " + Channel15Outputs [OutputMode].ToString ();
+			Output16Text.text = "16 - " + Channel16Outputs [OutputMode].ToString ();
+
+
+		}
+
+		for (int i = 0; i < keyCodes.Length; i++) {
+			if (Input.GetKeyDown (keyCodes [i])) {
+
+				int numberPressed = i;
+				Debug.Log (numberPressed);
+
+				if (MidiDevices.Count - 1 >= numberPressed) {
+					MidiDeviceID = numberPressed;
+					MidiDevice.TurnAllNotesOff ();
+					MidiDevice.Dispose ();
+					MidiDevice = OutputDevice.GetByName (MidiDevices [MidiDeviceID]);
+					DeviceNameText.text = MidiDevices [MidiDeviceID] + " (" + MidiDeviceID + ")";
+				}
+
+			}
+		}
+	}
+
+}
